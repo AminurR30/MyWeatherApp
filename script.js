cde22b5c1aa8506df6efbfdc8325e6c7895f5f12
Aminur Rahman <150610324+AminurR30@users.noreply.github.com>
2024-09-17 22:46:25 +0530

    Add country code

--- script.js
+++ script.js
@@ -31,12 +31,14 @@ let weather = {
   },
 
   displayWeather: function (data) {
-    const { name } = data;
+    const { name, sys } = data; // Destructure sys from data
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
 
-    document.querySelector(".city").innerText = `Weather in ${name}`;
+    document.querySelector(
+      ".city"
+    ).innerText = `Weather in ${name},${sys.country}`; // Include country code
     document.querySelector(
       ".icon"
     ).src = `https://openweathermap.org/img/wn/${icon}.png`;
@@ -154,7 +156,7 @@ document.querySelector(".search-bar").addEventListener("keyup", (event) => {
   }
 });
 
-weather.fetchWeather("Kolkata"); // Default city
+weather.fetchWeather("Kolkata,IN"); // Default city with country code
 
 // Update the date and time every minute
 setInterval(() => {
